99af78e0c33557ea069b0f1f02975fd61b6f0ee2
berli <libo22@gmail.com>
2018-12-10 10:21:33 +0800

    modified:   tf_variable_tb.py

--- tf3/tf_variable_tb.py
+++ tf3/tf_variable_tb.py
@@ -2,8 +2,8 @@
 import tensorflow as tf
 
 #定义变量空间,并且设置reuse标志为AUTO_RESUSE
-with tf.name_scope("weight"):
-    with tf.variable_scope("get_variable", reuse=tf.AUTO_REUSE):
+with tf.name_scope("weight_scope"):
+    with tf.variable_scope("variable_scope", reuse=tf.AUTO_REUSE):
         #使用随机函数生成2×3维的张量，标准差是1,随机种子是1
         w1 = tf.get_variable(initializer = tf.random_normal([2,3], stddev = 1, seed = 1),  name = 'w1')
         #创建第二个变量，我们故意使用上一个变量的值
@@ -11,7 +11,7 @@ with tf.name_scope("weight"):
         #创建第三个变量w3时，tensorflow发现w3不存在，便用initializer初始化器创建新的变量
         w3 = tf.get_variable(initializer = tf.ones([2,3]),  name = 'w3')
 
-with tf.name_scope("bias"):
+with tf.name_scope("bias_scope"):
     bias = tf.Variable(initial_value = tf.constant([[0.1, 0.2,0.3],[0.4,0.5,0.6]]))
     x = tf.constant([[0.3,0.5],[0.1,0.2]])
 
