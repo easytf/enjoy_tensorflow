896e4777979aff450ae89445f9d183f43efca9d4
berli <libo22@gmail.com>
2019-01-21 21:25:49 +0800

    deleted:    sigmoid.py 	renamed:    dropout.py -> tf.dropout.py 	renamed:    relu.py -> tf.relu.py 	modified:   tf.sigmoid.py 	deleted:    tf4.4_dropout.py

--- tf2.0/tf4/tf.sigmoid.py
+++ tf2.0/tf4/tf.sigmoid.py
@@ -1,18 +1,21 @@
-
+#!/usr/bin/python 
 #encoding:utf-8
-import tensorflow as tf
+import math
+import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib as mpl
-import matplotlib.pyplot as plt
-
-x = np.linspace(-10, 10)
-y = tf.sigmoid(x)
-tanh = tf.tanh(x)
-relu = tf.nn.relu(x)
-
+mpl.rcParams['axes.unicode_minus']=False
+ 
+def  sigmoid(x):
+    return 1.0 / (1.0 + np.exp(-x))
+ 
 fig = plt.figure(figsize=(6,4))
 ax = fig.add_subplot(111)
-
+ 
+x = np.linspace(-10, 10)
+y = sigmoid(x)
+tanh = 2*sigmoid(2*x) - 1
+ 
 plt.xlim(-11,11)
 plt.ylim(-1.1,1.1)
  
@@ -29,9 +32,8 @@ ax.yaxis.set_ticks_position('left')
 ax.spines['left'].set_position(('data',0))
 ax.set_yticks([-1,-0.9,-0.7,-0.5,-0.3,-0.1,0.1,0.3,0.5,0.7,0.9,1])
  
+#plt.plot(x,y,label="Sigmoid",color = "red")
 plt.plot(2*x,tanh,label="Tanh", color = "blue")
-plt.plot(x,y,label="Sigmoid",color = "red")
-plt.plot(x, relu,label="Relu",color = "green")
 plt.legend()
 plt.show()
 
