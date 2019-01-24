4d649cb06202ffae5193a0536a2bf742448acf49
berli <libo22@gmail.com>
2019-01-24 16:23:06 +0800

    modified:   tf_add_tb.py

--- tf2.0/tf3/tf_add_tb.py
+++ tf2.0/tf3/tf_add_tb.py
@@ -5,20 +5,21 @@ import tensorflow as tf
 
 a = tf.constant([1.0, 2.0], dtype=tf.float32, name = 'a')
 b = tf.constant([3.0, 4.0], dtype=tf.float32, name = 'b')
-c = a + b
 
 summary_writer = tf.summary.create_file_writer('log', flush_millis=1000)
-with summary_writer.as_default():
-    with tf.summary.summary_scope("add"):
-        tf.summary.write('c', c, 1)
-
-#summary_op = tf.summary.merge_all()
-#summary_writer = tf.summary.SummaryWriter('./log/')
-#summary_writer.add_summary(summary, 0)
+summary_writer.as_default()
 
 with tf.GradientTape() as g:
-    dy_dx = g.gradient(y, [b])
+    g.watch(a)
+    c = a + b
+    dy_dx = g.gradient(c, [a,b])
     print('a:',a)
     print('b:',b)
     print('c:',c)
     print('dy_dx:',dy_dx)
+    with tf.summary.summary_scope("add"):
+        tf.summary.write('c', a, 1)
+        tf.summary.write('c', b, 2)
+        tf.summary.write('c', c, 3)
+        tf.summary.write('c', dy_dx, 4)
+        tf.summary.flush()
