b83172a3d9b517ae82827c5b15dbcc89e57397bd
berli <libo22@gmail.com>
2019-01-24 17:35:25 +0800

    modified:   tf4.1.py 	modified:   tf4.2.py

--- tf2.0/tf4/tf4.1.py
+++ tf2.0/tf4/tf4.1.py
@@ -1,23 +1,22 @@
-
 import tensorflow as tf
 import numpy as np
-import random
 
 def product(x, y): 
     h = 0.01
-    z = ( x + random.random()*h )*( y + random.random()*h )
+    z = tf.multiply(( x + tf.random.normal(shape=[1])*h ), ( y + tf.random.normal(shape=[1])*h ))
     return z
 
 a = np.array([3]) 
 b = np.array([4]) 
-print(product(a, b))
+c = product(a, b)
+print( c.numpy())
 
 def product2(x, y): 
     h = 0.01
-    z = ( x + h*y )*( y + h*x )
-
+    z = tf.multiply(( x + h*y ),( y + h*x ))
     return z
 
 a = np.array([-3]) 
 b = np.array([-4]) 
-print(product2(a, b))
+c = product2(a, b)
+print( c.numpy())
--- tf2.0/tf4/tf4.2.py
+++ tf2.0/tf4/tf4.2.py
@@ -2,13 +2,13 @@ import tensorflow as tf
 import numpy as np
 
 def product(x, y): 
-    return x * y
+    return tf.multiply(x, y)
 
 def add(x, y): 
-    return (x + y) 
+    return tf.add(x, y) 
 
 def forward(a, b, c):
-    d = add(a, b)
+    d = tf.add(a, b)
     return product(d, c)
 
 a =np.array([5])
@@ -16,21 +16,21 @@ b =np.array([-3])
 c =np.array([-2])
 print(forward(a, b, c))
 
-d = add(a, b)
+d = tf.add(a, b)
 h = 0.01
-df_wrt_d = c
-df_wrt_c = d
-dd_wrt_a = 1
-dd_wrt_b = 1
+df_wrt_d = tf.cast(c, tf.float32)
+df_wrt_c = tf.cast(d, tf.float32)
+dd_wrt_a = 1.0
+dd_wrt_b = 1.0
 
-df_wrt_a = df_wrt_d*dd_wrt_a
-df_wrt_b = df_wrt_d*dd_wrt_b
+df_wrt_a = tf.multiply(df_wrt_d,dd_wrt_a)
+df_wrt_b = tf.multiply(df_wrt_d,dd_wrt_b)
 
 a = a + h*df_wrt_a
 b = b + h*df_wrt_b
 c = c + h*df_wrt_c
 def bakcforward(a, b, c):
-    d = add(a, b)
+    d = tf.add(a, b)
     return product(d, c)
 
 print(bakcforward(a, b,c ))
